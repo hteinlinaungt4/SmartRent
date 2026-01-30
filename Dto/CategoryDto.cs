4b152d1f72fbdf7edcc80d6fa5b88589758d0394
Htein Lin Aung <hteinlinaung@abccsmm.com>
2026-01-30 10:07:01 +0630

    Dto  Added

---
+++ Dto/CategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SmartRent.Dto
+{
+    // Category အသစ်ဆောက်ဖို့
+    public record CreateCategoryDto(string Name, string? IconName);
+
+    // Category ပြန်ထုတ်ပြဖို့ (Response)
+    public record CategoryResponseDto(int Id, string Name, string IconName);
+}
--- Dto/CategoryResponseDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SmartRent.Dto
-{
-    public class CategoryResponseDto
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string IconName { get; set; } = string.Empty;
-    }
-}
